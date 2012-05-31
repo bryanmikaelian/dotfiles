e480d978baeae77ccce1e90f1cc172d03b5cb83a
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2012-05-31 15:50:39 -0500

    E for editing

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -36,3 +36,6 @@ alias cdr="cd ~/Code/Rails"
 # postgres
 alias pgs="pg_ctl -D /usr/local/var/pgsql/data -l /usr/local/var/pgsql/server.log start"
 alias pgk="pg_ctl -D /usr/local/var/pgsql/data stop -s -m fast"
+
+# mvim
+alias e="mvim ."
