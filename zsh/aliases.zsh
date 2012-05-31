9b12c9eea837701b851e1ed1ffa9a3bad06c2dad
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2012-05-31 14:29:31 -0500

    Postgres aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -34,4 +34,5 @@ alias rgm="rails generate model"
 alias cdr="cd ~/Code/Rails"
 
 # postgres
-alias pgs="postgres -D /usr/local/var/pgsql/data"
+alias pgs="pg_ctl -D /usr/local/var/pgsql/data -l /usr/local/var/pgsql/server.log start"
+alias pgk="pg_ctl -D /usr/local/var/pgsql/data stop -s -m fast"
