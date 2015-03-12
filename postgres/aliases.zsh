da5fc41c8784aac4f47eebf9af38fc9fc86e645b
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2015-03-12 09:28:01 -0500

    Fish shell support

--- postgres/aliases.zsh
+++ postgres/aliases.zsh
@@ -1,2 +1,2 @@
-alias pgs="pg_ctl -D /usr/local/var/postgres/ -l /usr/local/var/pgsql/server.log start -w"
+alias pgs="pg_ctl -D /usr/local/var/pgsql/ -l /usr/local/var/pgsql/server.log start -w"
 alias pgk="pg_ctl -D /usr/local/var/postgres/ stop -s -m fast"
