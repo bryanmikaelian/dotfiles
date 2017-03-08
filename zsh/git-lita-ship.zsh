4931c826a96e1cf2a5effffb6bfe055aa48d0fbd
Bryan Mikaelian <bryan@librato.com>
2017-03-08 12:11:46 -0600

    Lita script change

--- zsh/git-lita-ship.zsh
+++ zsh/git-lita-ship.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 function git-lita-ship {
   TAG=$(git describe | cut -d '-' -f 1)
-  COMMAND="lita ship $TAG to $(basename $(pwd)) production"
+  COMMAND="lita shipit -b $TAG -a $(basename $(pwd)) -e production"
   echo -n "$COMMAND" | pbcopy
   echo "\"$COMMAND\" copied to clipboard."
 }
