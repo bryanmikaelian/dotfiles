a3d09ab05db50b8b05da1d8d65b6b745316161d4
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2017-07-24 08:21:57 -0500

    Fix copy paste in tmux

--- work/librato.zsh
+++ work/librato.zsh
@@ -7,12 +7,12 @@ export SL_API_TEST_DB="mysql2://metrics:@127.0.0.1/metrics_test"
 export SL_API_TSSTORE_URI=cassandra://127.0.0.1
 export SL_API_URI="http://0.0.0.0:9292"
 
-export METRICS_API_SITE="http://api.librato.dev:9292"
-export METRICS_TEST_API_SITE="http://api.librato.dev:9292"
-export SPACES_API_URI="http://api.librato.dev:8081"
-export SPACES_TEST_API_URI="http://api.librato.dev:9292"
-export SPACES_DIRECT_API="http://api.librato.dev:9292"
-export SDK_API_URI="http://api.librato.dev:9292"
+export METRICS_API_SITE="http://0.0.0.0:9292"
+export METRICS_TEST_API_SITE="http://0.0.0.0:9292"
+export SPACES_API_URI="http://0.0.0.0:9292"
+export SPACES_TEST_API_URI="http:/0.0.0.0:9292"
+export SPACES_DIRECT_API="http://0.0.0.0:9292"
+export SDK_API_URI="http://0.0.0.0:9292"
 # export MEMCACHE_URI="http://0.0.0.0:11211"
 export QC_DATABASE_URL="postgres://librato_worker:@localhost:5432/librato_workers"
 
