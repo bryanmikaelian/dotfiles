a9ad57fe9080ab2ccb67b2d414f2d02a7bac52bc
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2019-05-08 07:49:51 -0400

    Clean up git aliases. Add to fish

--- config/config.symlink/fish/config.fish
+++ config/config.symlink/fish/config.fish
@@ -6,3 +6,16 @@ set -xU GOROOT /usr/local/opt/go/libexec
 set -xU GOPATH $HOME/Code/Go
 
 status --is-interactive; and source (rbenv init -|psub)
+
+alias gl='git pull'
+alias gp='git push'
+alias gcl='git clean -f'
+alias gd='git diff'
+alias gca='git commit -a'
+alias gco='git checkout'
+alias gb='git branch'
+alias gs='git status'
+alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias changelog='git log `git log -1 --format=%H -- CHANGELOG*`..; cat CHANGELOG*'
+alias gsiu='git submodule init && git submodule update'
+alias gsu='git submodule foreach git pull'
--- git/aliases.zsh
+++ git/aliases.zsh
@@ -10,22 +10,3 @@ alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
 alias changelog='git log `git log -1 --format=%H -- CHANGELOG*`..; cat CHANGELOG*'
 alias gsiu='git submodule init && git submodule update'
 alias gsu='git submodule foreach git pull'
-
-function git-tag-next {
-    PREV_TAG=`git describe | cut -d '-' -f 1`;
-    TAG_PREFIX=`echo $PREV_TAG | sed -e 's/[0-9][0-9]*$//'`;
-    CURR_TAG=`echo $PREV_TAG | sed -e 's/^[^0-9]*//'`;
-    if [ -z $CURR_TAG ]; then
-        echo "Can not parse tag!";
-        return 1;
-    fi;
-    NEW_TAG=$(( $CURR_TAG + 1 ));
-    NEW_TAG="${TAG_PREFIX}${NEW_TAG}";
-    echo "Bumping tag from ${PREV_TAG} -> ${NEW_TAG}";
-    MSG="$1";
-    if [ -z "$MSG" ]; then
-        MSG=$NEW_TAG;
-    fi;
-    git tag -a $NEW_TAG -m "${MSG}";
-    # git push origin ${NEW_TAG}
-}
