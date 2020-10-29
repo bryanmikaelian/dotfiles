e84f62ad12806d7e6e6ffec352133da2ca18ea8b
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2020-10-29 10:14:24 -0400

    Google Cloud SDK

---
+++ zsh/gcsdk.zsh
@@ -0,0 +1,5 @@
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/bryanmikaelian/.gce/path.zsh.inc' ]; then . '/Users/bryanmikaelian/.gce/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/Users/bryanmikaelian/.gce/completion.zsh.inc' ]; then . '/Users/bryanmikaelian/.gce/completion.zsh.inc'; fi
