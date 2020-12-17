925bb17f70ccf91367e63da4236d35dd1d4ba33a
Bryan Mikaelian <bryan@netlify.com>
2020-12-17 15:59:25 -0500

    Neovim + Night Owl + Tmux fixes

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,7 @@
+# editor
+alias e=nvim
+alias vim=nvim
+
 # cd
 alias ..='cd ..'
 alias c="cd ~/Code"
@@ -13,13 +17,11 @@ alias la='ls -A'
 alias mdf="cd ~/.dotfiles && mvim ~/.dotfiles"
 
 # tmux
-alias tmux="TERM=screen-256color-bce tmux"
-alias mux="TERM=screen-256color-bce mux"
+alias tmux="tmux -2"
 alias tmuxkill="tmux kill-session"
 
 # DBs
-alias sup="mysqlstart; rup"
-
+alias sup="mysqlstart; rup" 
 # Docker
 alias dockercleanup="docker rm `docker ps -aq`"
 
@@ -35,3 +37,4 @@ alias kc="kubectl"
 # netlify
 alias bbstg="gcloud container clusters get-credentials netlify-staging-dsm-1 --region us-central1 --project staging-267123; kubectl exec -n bitballoon-staging -it $(kubectl -n bitballoon-staging get pods --selector role=bitballoon-api -o jsonpath='{.items[0].metadata.name}') -- bash rails-console.sh"
 alias bbsupport="~/Code/github.com/netlify/bitballoon/kubernetes/support-env.sh"
+
