0f2ef5ee405a60862b9e18c9687255bc7cfb9b90
Bryan Mikaelian <bryan@netlify.com>
2023-03-14 09:33:27 -0400

    Sync

--- config/nvim.symlink/lua/custom/chadrc.lua
+++ config/nvim.symlink/lua/custom/chadrc.lua
@@ -1,16 +1,11 @@
 local M = {}
 
--- make sure you maintain the structure of `core/default_config.lua` here,
--- example of changing theme:
-
 M.ui = {
    theme = "onenord"
 }
 
--- so setup_lspconf = any file but that should be in custom dir!
 M.plugins = require "custom.plugins"
-
 M.mappings = require "custom.mappings"
 
-return M
 
+return M
