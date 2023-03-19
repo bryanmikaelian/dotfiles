1e8c6e90ac5f130e02ef3427f9233b727a212502
Bryan Mikaelian <bryan@netlify.com>
2023-03-19 13:40:50 -0400

    Plugin updates

--- config/nvim.symlink/lua/custom/mappings.lua
+++ config/nvim.symlink/lua/custom/mappings.lua
@@ -1,3 +1,13 @@
 local M = {}
 
+M.vim_test = {
+  n = {
+    ["<leader>tt"] = { ":TestNearest<CR>", "Test Nearest" },
+    ["<leader>tT"] = { ":TestFile<CR>", "Test File" },
+    ["<leader>ta"] = { ":TestSuite<CR>", "Test Suite" },
+    ["<leader>tl"] = { ":TestLast<CR>", "Test Last" },
+    ["<leader>tg"] = { ":TestVisit<CR>", "Test Visit" },
+  }
+}
+
 return M
