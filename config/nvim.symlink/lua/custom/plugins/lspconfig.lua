6c0f4f43171855d9be48dadd96b0bb6b89a7977d
Bryan Mikaelian <bryan@netlify.com>
2023-04-10 12:37:38 -0400

    Typescript

--- config/nvim.symlink/lua/custom/plugins/lspconfig.lua
+++ config/nvim.symlink/lua/custom/plugins/lspconfig.lua
@@ -4,7 +4,7 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "solargraph", "clojure_lsp", "eslint", "gopls"}
+local servers = { "solargraph", "clojure_lsp", "eslint", "gopls", "tsserver" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
