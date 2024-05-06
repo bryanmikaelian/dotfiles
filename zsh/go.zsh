0c2c511e86a5573c27a01619e00c77777a10832a
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2024-05-06 15:54:49 -0400

    Updates

--- zsh/go.zsh
+++ zsh/go.zsh
@@ -1,2 +1,5 @@
 # go binaries
-export PATH="$PATH:$(go env GOPATH)/bin"
+# export PATH="$PATH:$(go env GOPATH)/bin"
+export PATH="$PATH:/opt/homebrew/opt/go@1.20/bin"
+export GOPATH="$HOME/go"
+export PATH="$PATH:$GOPATH/bin"
