9f6b6bf921b6555e21c416a68acae5eeb7b7aba9
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2024-05-15 05:57:25 -0400

    Updates

--- config/nvim.symlink/lua/config/keymaps.lua
+++ config/nvim.symlink/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- unbind location and quickfix options
+local map = LazyVim.safe_keymap_set
+
+map("n", "<leader>xl", "<Nop>")
+map("n", "<leader>xq", "<Nop>")
---
+++ config/nvim.symlink/lua/plugins/test.lua
@@ -0,0 +1,99 @@
+-- TOOD: workaround until
+-- https://github.com/nvim-neotest/neotest-go/issues/83 is fixed
+local function get_nearest_function_name()
+  local ts_utils = require("nvim-treesitter.ts_utils")
+  local node = ts_utils.get_node_at_cursor()
+
+  while node do
+    if node:type() == "function_declaration" then
+      return ts_utils.get_node_text(node:child(1))[1]
+    end
+    node = node:parent()
+  end
+end
+
+return {
+  { "nvim-neotest/neotest-plenary" },
+  { "nvim-neotest/neotest-go" },
+  {
+    "nvim-neotest/neotest",
+    opts = {
+      adapters = {
+        ["neotest-go"] = {},
+      },
+      status = { virtual_text = true },
+      output = { open_on_run = true },
+      quickfix = {
+        open = function()
+          if LazyVim.has("trouble.nvim") then
+            require("trouble").open({ mode = "quickfix", focus = false })
+          else
+            vim.cmd("copen")
+          end
+        end,
+      },
+    },
+    keys = {
+      {
+        "<leader>tt",
+        function()
+          require("neotest").run.run(vim.fn.expand("%"))
+        end,
+        desc = "Run File",
+      },
+      {
+        "<leader>tT",
+        function()
+          require("neotest").run.run(vim.uv.cwd())
+        end,
+        desc = "Run All Test Files",
+      },
+      {
+        "<leader>tr",
+        function()
+          local name = get_nearest_function_name()
+          if not name then
+            return
+          end
+          require("neotest").run.run({ extra_args = { "-run", name } })
+        end,
+        desc = "Run Nearest",
+      },
+      {
+        "<leader>tl",
+        function()
+          require("neotest").run.run_last()
+        end,
+        desc = "Run Last",
+      },
+      {
+        "<leader>ts",
+        function()
+          require("neotest").summary.toggle()
+        end,
+        desc = "Toggle Summary",
+      },
+      {
+        "<leader>to",
+        function()
+          require("neotest").output.open({ enter = true, auto_close = true })
+        end,
+        desc = "Show Output",
+      },
+      {
+        "<leader>tO",
+        function()
+          require("neotest").output_panel.toggle()
+        end,
+        desc = "Toggle Output Panel",
+      },
+      {
+        "<leader>tS",
+        function()
+          require("neotest").run.stop()
+        end,
+        desc = "Stop",
+      },
+    },
+  },
+}
