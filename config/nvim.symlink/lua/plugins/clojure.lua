47336ee930ddd8e1cce37f04fb0383699f83a6ed
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2024-07-17 19:24:10 -0400

    Clojure

--- config/nvim.symlink/lua/plugins/clojure.lua
+++ config/nvim.symlink/lua/plugins/clojure.lua
@@ -11,17 +11,17 @@ return {
     "hiphish/rainbow-delimiters.nvim",
     ft = { "clojure", "clojurescript" },
   },
-
+  {
+    "PaterJason/cmp-conjure",
+    ft = { "clojure", "clojurescript" },
+  },
   {
     "hrsh7th/nvim-cmp",
     optional = true,
     dependencies = {
       "PaterJason/cmp-conjure",
     },
-    opts = function(_, opts)
-      if type(opts.sources) == "table" then
-        vim.list_extend(opts.sources, { name = "clojure" })
-      end
-    end,
+    ft = { "clojure", "clojurescript" },
+    opts = function(_, opts) end,
   },
 }
