26673ef2fb9c85447ccba5cdd7160bcef98a034d
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2024-08-01 20:53:45 -0400

    New plugins

--- config/nvim.symlink/lua/plugins/neotest.lua
+++ config/nvim.symlink/lua/plugins/neotest.lua
@@ -1,99 +1,37 @@
--- TOOD: workaround until
--- https://github.com/nvim-neotest/neotest-go/issues/83 is fixed
-local function get_nearest_function_name()
-  local ts_utils = require("nvim-treesitter.ts_utils")
-  local node = ts_utils.get_node_at_cursor()
-
-  while node do
-    if node:type() == "function_declaration" then
-      return ts_utils.get_node_text(node:child(1))[1]
-    end
-    node = node:parent()
-  end
-end
-
 return {
-  { "nvim-neotest/neotest-plenary" },
-  { "nvim-neotest/neotest-go" },
   {
     "nvim-neotest/neotest",
-    opts = {
-      adapters = {
-        ["neotest-go"] = {},
+
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      {
+        "fredrikaverpil/neotest-golang",
+        dependencies = {
+          "leoluz/nvim-dap-go",
+        },
       },
-      status = { virtual_text = true },
-      output = { open_on_run = true },
-      quickfix = {
-        open = function()
-          if LazyVim.has("trouble") then
-            require("trouble").open({ mode = "quickfix", focus = false })
-          else
-            vim.cmd("copen")
-          end
-        end,
-      },
+      "nvim-neotest/neotest-jest",
     },
-    keys = {
-      {
-        "<leader>tt",
-        function()
-          require("neotest").run.run(vim.fn.expand("%"))
-        end,
-        desc = "Run File",
-      },
-      {
-        "<leader>tT",
-        function()
-          require("neotest").run.run(vim.uv.cwd())
-        end,
-        desc = "Run All Test Files",
-      },
-      {
-        "<leader>tr",
-        function()
-          local name = get_nearest_function_name()
-          if not name then
-            return
-          end
-          require("neotest").run.run({ extra_args = { "-run", name } })
-        end,
-        desc = "Run Nearest",
-      },
-      {
-        "<leader>tl",
-        function()
-          require("neotest").run.run_last()
-        end,
-        desc = "Run Last",
-      },
-      {
-        "<leader>ts",
-        function()
-          require("neotest").summary.toggle()
-        end,
-        desc = "Toggle Summary",
-      },
-      {
-        "<leader>to",
-        function()
-          require("neotest").output.open({ enter = true, auto_close = true })
-        end,
-        desc = "Show Output",
-      },
-      {
-        "<leader>tO",
-        function()
-          require("neotest").output_panel.toggle()
-        end,
-        desc = "Toggle Output Panel",
-      },
-      {
-        "<leader>tS",
-        function()
-          require("neotest").run.stop()
-        end,
-        desc = "Stop",
-      },
-    },
+    config = function()
+      require("neotest").setup({
+        adapters = {
+          -- golang
+          require("neotest-golang"),
+
+          -- javascript
+          require("neotest-jest")({
+            jestCommand = "npm test --",
+            jestConfigFile = "custom.jest.config.ts",
+            env = { CI = true },
+            cwd = function(path)
+              return vim.fn.getcwd()
+            end,
+          }),
+        },
+      })
+    end,
   },
 }
