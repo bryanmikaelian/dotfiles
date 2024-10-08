90af0e746b63c5d139d0632ad2989b57b809a004
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2024-10-08 10:37:56 -0400

    Updates. Add work.lua file to gitignore

--- config/doom.symlink/custom.el
+++ config/doom.symlink/custom.el
@@ -6,7 +6,7 @@
  '(custom-safe-themes
    '("691d671429fa6c6d73098fc6ff05d4a14a323ea0a18787daeb93fde0e48ab18b" "00cec71d41047ebabeb310a325c365d5bc4b7fab0a681a2a108d32fb161b4006" default))
  '(org-agenda-files
-   '("/Users/bryanmikaelian/.org/op/proj/disable-family-to-business.org" "/Users/bryanmikaelian/.org/op/index.org" "/Users/bryanmikaelian/.org/index.org")))
+   '("~/.org/op/proj/tsp-additional-members.org" "/Users/bryanmikaelian/.org/op/proj/disable-family-to-business.org" "/Users/bryanmikaelian/.org/op/index.org" "/Users/bryanmikaelian/.org/index.org")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
