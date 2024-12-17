5ccddb629607fbf8f478cea51b00778d4c7319df
Bryan Mikaelian <bryan.mikaelian@agilebits.com>
2024-12-17 09:51:52 -0500

    Updates

---
+++ config/nvim.symlink/lua/plugins/colorscheme.lua
@@ -0,0 +1,20 @@
+return {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "neanias/everforest-nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("everforest").setup({
+        background = "soft",
+      })
+    end,
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "everforest",
+    },
+  },
+}
