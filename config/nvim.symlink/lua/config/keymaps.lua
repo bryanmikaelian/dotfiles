22fd3a19ab8247837458863104b06d622a52e560
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2026-02-06 20:56:24 -0500

    Fix smart-splits

--- config/nvim.symlink/lua/config/keymaps.lua
+++ config/nvim.symlink/lua/config/keymaps.lua
@@ -2,6 +2,19 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- Delete LazyVim's default window navigation keymaps (we use smart-splits instead)
+vim.keymap.del("n", "<C-h>")
+vim.keymap.del("n", "<C-j>")
+vim.keymap.del("n", "<C-k>")
+vim.keymap.del("n", "<C-l>")
+
+-- Re-set smart-splits keymaps after LazyVim overwrites them
+local smart_splits = require("smart-splits")
+vim.keymap.set("n", "<C-h>", smart_splits.move_cursor_left)
+vim.keymap.set("n", "<C-j>", smart_splits.move_cursor_down)
+vim.keymap.set("n", "<C-k>", smart_splits.move_cursor_up)
+vim.keymap.set("n", "<C-l>", smart_splits.move_cursor_right)
+
 -- unbind location and quickfix options
 local fzf = require("fzf-lua")
 
--- config/nvim.symlink/lua/plugins/splits.lua
+++ config/nvim.symlink/lua/plugins/splits.lua
@@ -2,4 +2,21 @@ return {
   "mrjones2014/smart-splits.nvim",
   build = "./kitty/install-kittens.bash",
   lazy = false,
+  config = function()
+    require("smart-splits").setup({
+      multiplexer_integration = "kitty",
+    })
+
+    -- Navigation between vim splits and kitty windows
+    vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
+    vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
+    vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
+    vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
+
+    -- Resize vim splits (matching your kitty alt+hjkl config)
+    vim.keymap.set("n", "<A-h>", require("smart-splits").resize_left)
+    vim.keymap.set("n", "<A-j>", require("smart-splits").resize_down)
+    vim.keymap.set("n", "<A-k>", require("smart-splits").resize_up)
+    vim.keymap.set("n", "<A-l>", require("smart-splits").resize_right)
+  end,
 }
